a63ddd0383c374240605944e809b233c2f3871c8
Oussama Mejjati <badrmejjati2011@gmail.com>
2025-07-24 12:09:12 +0100

    only navbar but incomplete

---
+++ resources/views/home.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <title>Commune M'talssa</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-light bg-light position-relative" style="height: 60px; overflow: visible;">
+    <div class="container-fluid d-flex justify-content-between align-items-center">
+
+        <ul class="nav mb-0">
+            <li class="nav-item">
+                <a href="#" class="nav-link">أخبار الجماعة</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">التواصل</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">رابط</a>
+            </li>
+        </ul>
+
+        <!-- Logo (taller than navbar) -->
+        <a href="#" class="navbar-brand p-0 m-0" style="position: absolute; top: 0px; right: 20px;" >
+            <img src="{{ asset('images/mtalssa-logo.png') }}" alt="شعار الجماعة" id="logo"id="logo">
+        </a>
+
+    </div>
+</nav>
+
+
+</body>
+
+</html>
