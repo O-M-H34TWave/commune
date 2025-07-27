7a1a09a4d01c5e3a7543c89bb475055cd7cf2d6b
Oussama <badrmejjati2011@gmail.com>
2025-07-27 16:07:13 +0100

    changed maps pics

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -95,10 +95,21 @@
                 </div>
             </div>
         </div>
-        <div>
-            <h1 class="float-end mb-5">نطاق الجماعة<i class="bi bi-grip-vertical"></i>
+        <div class="mb-5 text-end">
+            <h1>معلومات عامة عن الجماعة <i class="bi bi-grip-vertical"></i></h1>
+            <p>تعتبر <b> جماعة امطالسة </b>بإقليم الدريوش أكبر جماعة من ناحية المساحة، حيث تشكل <b>539.3 كم²</b>، و
+                تتبعها <b>جماعة عين الزهرة ب311.8 كم²</b> ثم <b>جماعة اولاد بوبكر ب281.6 كم²</b>.
+                عدد سكان جماعة امطالسة هو 16،778 نسمة حسب احصاء 2024، وكثافتها السكانية 31.11 نسمة في كم².
+            </p>
+        </div>
+        <div class="mb-5 text-end">
+            <h1>نطاق الجماعة<i class="bi bi-grip-vertical"></i>
             </h1>
-            <img src="{{asset('images/mtalssa.png')}}" alt="">
+            <a href="https://www.google.com/maps/place/Mtalssa/@34.8873494,-3.3965004,47135m/data=!3m1!1e3!4m6!3m5!1s0xd76422aa453d88f:0x4d5999ddafe69e67!8m2!3d34.9692825!4d-3.3690572!16s%2Fg%2F120_g1gq!5m1!1e4?entry=ttu&g_ep=EgoyMDI1MDcyMy4wIKXMDSoASAFQAw%3D%3D"
+                target="_blank">
+
+                <img src="{{asset('images/mtalssa.png')}}" alt="" class="img-fluid w-100">
+            </a>
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
